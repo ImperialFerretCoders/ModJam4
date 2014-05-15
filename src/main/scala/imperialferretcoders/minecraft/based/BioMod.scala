85333fc2735493362f499128e3556ef6c48538a8
Gyro <gyro@zettabyte.ws>
2014-05-14 21:59:08 -0400

    Mod object

---
+++ src/main/scala/imperialferretcoders/minecraft/based/BioMod.scala
@@ -0,0 +1,25 @@
+package imperialferretcoders.minecraft.based
+
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.Mod.Instance
+import cpw.mods.fml.common.event.FMLPreInitializationEvent
+import cpw.mods.fml.common.event.FMLPostInitializationEvent
+import cpw.mods.fml.common.event.FMLInitializationEvent
+
+@Mod(modid = "based", name = "Bio-Adaptive Synthetic Environment Deployment", version = "0.0.0", modLanguage = "scala")
+object BioMod {
+  @Mod.EventHandler
+  def PreInit(event: FMLPreInitializationEvent) {
+
+  }
+  @Mod.EventHandler
+  def init(event: FMLInitializationEvent) {
+
+  }
+  @Mod.EventHandler
+  def init(event: FMLPostInitializationEvent) {
+
+  }
+}
