a255bd3e74f372666fdce7b4b492df91cdb8f57b
Nathaniel Baxter <nathaniel.baxter@gmail.com>
2014-05-16 07:44:36 +0000

    Commented out broken code in TileEntityStem

--- src/main/scala/imperialferretcoders/minecraft/based/tileentities/TileEntityStem.scala
+++ src/main/scala/imperialferretcoders/minecraft/based/tileentities/TileEntityStem.scala
@@ -2,10 +2,10 @@ package imperialferretcoders.minecraft.based.tileentities
 
 import net.minecraft.tileentity.TileEntity
 
-import imperialferretcoders.minecraft.based.genetics.{BlockGenome, OrganismState}
+//import imperialferretcoders.minecraft.based.genetics.{BlockGenome, OrganismState}
 
 class TileEntityStem extends TileEntity {
-  protected var orgState: OrganismState = OrganismState()
+  //protected var orgState: OrganismState = OrganismState()
   //Must be assigned later.
-  protected var genome: BlockGenome = null();
+  //protected var genome: BlockGenome = null();
 }
